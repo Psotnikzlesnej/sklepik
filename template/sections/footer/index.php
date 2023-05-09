8c02e508ad2778fc2a8df7b433d889b10f9b0072
Nikrop <ltomasz989@gmail.com>
2023-05-09 09:49:49 +0200

    #7
    Dodanie kodu do index.php i style.scss

---
+++ template/sections/footer/index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <footer class="footer">
+        <ul class="footer__list">
+            <li class="footer__listItem">
+                <p class="footer__heading">INFORMACJE</p>
+                <div class="footer__line">
+                    <span class="footer__lineColorRed line"></span>
+                    <span class="footer__lineColorWhite line"></span>
+                </div>
+                <ul class="footer__nestedList">
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Informacje o sklepie</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Porady</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Blog</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Galeria inspiracji</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Kontakt</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Informacje o sklepie</a></p>
+                    </li>
+                </ul>
+            </li>
+            <li class="footer__listItem information">
+                <p class="footer__heading">INFORMACJE</p>
+                <div class="footer__line">
+                    <span class="footer__lineColorRed line"></span>
+                    <span class="footer__lineColorWhite line"></span>
+                </div>
+                <ul class="footer__nestedList">
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Czas realizacji</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Koszty przesyłki</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Zamów próbkę</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Szycie na wymiar</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Czas realizacji</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Koszty przesyłki</a></p>
+                    </li>
+                </ul>
+            </li>
+            <li class="footer__listItem information">
+                <p class="footer__heading">MOJE KONTO</p>
+                <div class="footer__line">
+                    <span class="footer__lineColorRed line"></span>
+                    <span class="footer__lineColorWhite line"></span>
+                </div>
+                <ul class="footer__nestedList">
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Logowanie</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Rejestracja</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Zamównienia</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Ustawienia konta</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Zmiana hasła</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Schowek</a></p>
+                    </li>
+                </ul>
+            </li>
+            <li class="footer__listItem information">
+                <p class="footer__heading">INFORMACJE</p>
+                <div class="footer__line">
+                    <span class="footer__lineColorRed line"></span>
+                    <span class="footer__lineColorWhite line"></span>
+                </div>
+                <ul class="footer__nestedList">
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Informacje o sklepie</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Porady</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Blog</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Galeria inspiracji</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Kontakt</a></p>
+                    </li>
+                    <li class="footer__nestedListItem">
+                        <p class="footer__nestedListItemParagraph"><a href="#" class="footer__nestedListItemAnchor">Informacje o sklepie</a></p>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </footer>
+    <div class="copyright">
+        <ul class="copyright__list">
+            <li class="copyright__listItem">Copyright 2022. Wszystkie prawa zastrzeżone</li>
+            <li class="copyright__listItem">Agencja interaktywna [ti] Powered by 2ClickShop</li>
+        </ul>
+    </div>
+</body>
+</html>
