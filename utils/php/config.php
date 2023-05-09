c113ea0c6c596015155ff5502d3934d8489c1e87
canicjusz <janek.bananek371@gmail.com>
2023-05-09 11:38:20 +0200

    podlaczone super lux

---
+++ utils/php/config.php
@@ -0,0 +1,25 @@
+<?php 
+$default_credentials = [
+  'HOSTNAME' => 'localhost', 
+  'USERNAME' => 'roots', 
+  'PASSWORD' => '', 
+  'DATABASE' => 'sklep'
+];
+
+$env_path = __DIR__ . '/../../.env';
+$credentials = file_exists($env_path) ? parse_ini_file($env_path) : $default_credentials;
+
+try {
+  $mysqli = @new mysqli(
+    $credentials['HOSTNAME'], 
+    $credentials['USERNAME'], 
+    $credentials['PASSWORD'], 
+    $credentials['DATABASE']
+  );
+  $mysqli->set_charset('utf8mb4');
+  echo '<b>success!</b>';
+} catch (Exception $e) {
+  echo '<b>Connection to the DB failed:</b> ' . $e;
+}
+
+?>
