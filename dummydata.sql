b167a91db294199e13bc71207e19badd30a073ed
SealSneezy <jakub.wypychowski@onet.pl>
2023-05-10 09:57:24 +0200

    #13 uzupełnienie pozostałych danych, dodanie sql'a z dummydata

--- dummydata.sql
+++ dummydata.sql
@@ -1,46 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Czas generowania: 10 Maj 2023, 09:14
--- Wersja serwera: 10.4.24-MariaDB
--- Wersja PHP: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Baza danych: `sklep`
---
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `banner`
---
-
-CREATE TABLE `banner` (
-  `ID` int(11) NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `image_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `link` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `visible` tinyint(1) DEFAULT 1,
-  `type` enum('home_top','home_tile','home_bottom','store') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `alt` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Zrzut danych tabeli `banner`
---
 
 INSERT INTO `banner` (`ID`, `title`, `description`, `image_name`, `link`, `visible`, `type`, `alt`) VALUES
 (1, 'Divanoodle', 'vel augue vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae donec pharetra magna vestibulum aliquet ultrices erat tortor sollicitudin mi sit amet lobortis sapien sapien non mi integer ac neque duis bibendum morbi non q', 'http://dummyimage.com/182x100.png/ff4444/ffffff', 'http://slashdot.org/in/magna.png?consequat=ac&morbi=consequat&a=metus&ipsum=sapien&integer=ut&a=nunc&nibh=vestibulum&in=ante&quis=ipsum&justo=primis&maecenas=in&rhoncus=faucibus&aliquam=orci&lacus=luctus&morbi=et&quis=ultrices&tortor=posuere&id=cubilia&nu', 0, 'home_top', 'nulla facilisi cras non velit nec'),
@@ -52,26 +9,6 @@ INSERT INTO `banner` (`ID`, `title`, `description`, `image_name`, `link`, `visib
 (7, 'Nlounge', 'lorem ipsum dolor sit amet consectetuer adipiscing elit proin risus praesent lectus vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae duis faucibus accumsan', 'http://dummyimage.com/156x100.png/cc0000/ffffff', 'https://guardian.co.uk/curabitur/convallis/duis/consequat.xml?cum=eu&sociis=sapien&natoque=cursus&penatibus=vestibulum&et=proin&magnis=eu&dis=mi&parturient=nulla&montes=ac&nascetur=enim&ridiculus=in&mus=tempor&vivamus=turpis&vestibulum=nec&sagittis=euismo', 1, 'home_top', 'vivamus vestibulum sagittis sapien cum sociis natoque'),
 (8, 'Meeveo', 'suspendisse ornare consequat lectus in est risus auctor sed tristique in tempus sit amet sem fusce consequat nulla nisl nunc nisl duis bibendum felis sed interdum venenatis turpis enim blandit mi in porttitor pede justo eu massa donec dapibus duis at veli', 'http://dummyimage.com/186x100.png/cc0000/ffffff', 'https://behance.net/fusce/congue/diam.jsp?potenti=faucibus&cras=accumsan&in=odio&purus=curabitur&eu=convallis&magna=duis&vulputate=consequat&luctus=dui&cum=nec&sociis=nisi&natoque=volutpat&penatibus=eleifend&et=donec&magnis=ut&dis=dolor&parturient=morbi&m', 0, 'home_bottom', 'in blandit ultrices enim lorem ipsum dolor');
 
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `category`
---
-
-CREATE TABLE `category` (
-  `ID` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `image_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `parent` int(11) DEFAULT NULL,
-  `level` int(11) DEFAULT NULL,
-  `type` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `description` varchar(512) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Zrzut danych tabeli `category`
---
-
 INSERT INTO `category` (`ID`, `name`, `image_name`, `parent`, `level`, `type`, `description`) VALUES
 (1, 'Cogilith', 'http://dummyimage.com/168x100.png/5fa2dd/ffffff', NULL, 1, 'justo etiam pretium', 'consequat dui nec nisi volutpat eleifend donec ut'),
 (2, 'Dynazzy', 'http://dummyimage.com/117x100.png/cc0000/ffffff', NULL, 1, 'accumsan tortor quis', 'viverra diam vitae quam suspendisse potenti nullam porttitor'),
@@ -81,21 +18,6 @@ INSERT INTO `category` (`ID`, `name`, `image_name`, `parent`, `level`, `type`, `
 (6, 'Gigashots', 'http://dummyimage.com/140x100.png/dddddd/000000', 3, 2, 'blandit lacinia erat', 'lectus in est risus auctor sed tristique in tempus sit'),
 (7, 'Dynava', 'http://dummyimage.com/123x100.png/dddddd/000000', 4, 2, 'eu felis fusce', 'id consequat in consequat ut nulla sed'),
 (8, 'Gabvine', 'http://dummyimage.com/107x100.png/ff4444/ffffff', 6, 3, 'nulla tellus in', 'potenti nullam porttitor lacus at turpis donec posuere metus');
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `delivery`
---
-
-CREATE TABLE `delivery` (
-  `ID` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Zrzut danych tabeli `delivery`
---
 
 INSERT INTO `delivery` (`ID`, `name`) VALUES
 (1, '24'),
@@ -107,22 +29,6 @@ INSERT INTO `delivery` (`ID`, `name`) VALUES
 (7, '24'),
 (8, '24');
 
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `filter`
---
-
-CREATE TABLE `filter` (
-  `ID` int(11) NOT NULL,
-  `type` enum('manufacturer','color','grid_multiple','range','slider') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Zrzut danych tabeli `filter`
---
-
 INSERT INTO `filter` (`ID`, `type`, `name`) VALUES
 (1, 'color', 'Trupe'),
 (2, 'color', 'Oba'),
@@ -133,32 +39,15 @@ INSERT INTO `filter` (`ID`, `type`, `name`) VALUES
 (7, 'grid_multiple', 'Zoomzone'),
 (8, 'grid_multiple', 'Voonder');
 
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `filter_value`
---
-
-CREATE TABLE `filter_value` (
-  `ID` int(11) NOT NULL,
-  `ID_filter` int(11) DEFAULT NULL,
-  `value` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `flag`
---
-
-CREATE TABLE `flag` (
-  `ID` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Zrzut danych tabeli `flag`
---
+INSERT INTO `filter_value` (`ID`, `ID_filter`, `value`) VALUES
+(1, 1, 'yellow'),
+(2, 2, 'red'),
+(3, 3, '25'),
+(4, 4, '100'),
+(5, 5, '50'),
+(6, 6, '75'),
+(7, 7, '90'),
+(8, 8, '100');
 
 INSERT INTO `flag` (`ID`, `name`) VALUES
 (1, 'new'),
@@ -170,42 +59,13 @@ INSERT INTO `flag` (`ID`, `name`) VALUES
 (7, 'promo'),
 (8, 'promo');
 
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `parameter_value`
---
-
-CREATE TABLE `parameter_value` (
-  `ID` int(11) NOT NULL,
-  `value` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `parameter` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `product`
---
-
-CREATE TABLE `product` (
-  `ID` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `video_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `promo_price` decimal(10,0) DEFAULT NULL,
-  `catalog_price` decimal(10,0) DEFAULT NULL,
-  `serial_number` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `stock` int(11) DEFAULT NULL,
-  `delivery_ID` int(11) DEFAULT NULL,
-  `visible` tinyint(1) DEFAULT 1,
-  `variant_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `variant_ID` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Zrzut danych tabeli `product`
---
+INSERT INTO `parameter_value` (`ID`, `value`, `parameter`) VALUES
+(1, '33', 'eget'),
+(2, '21', 'velit vivamus'),
+(3, '36', 'metus'),
+(4, '110', 'nisi'),
+(5, '56', 'pede lobortis'),
+(6, '60', 'ultrices');
 
 INSERT INTO `product` (`ID`, `name`, `description`, `video_url`, `promo_price`, `catalog_price`, `serial_number`, `stock`, `delivery_ID`, `visible`, `variant_name`, `variant_ID`) VALUES
 (1, 'Granger', 'est risus auctor sed tristique in tempus sit amet sem', 'http://dummyimage.com/166x100.png/ff4444/ffffff', '55', '22', '4237e8dsx793', 89, 8, 1, 'amet diam in magna bibendum imperdiet nullam orci pede venenatis non sodales sed tincidunt', 1),
@@ -217,55 +77,35 @@ INSERT INTO `product` (`ID`, `name`, `description`, `video_url`, `promo_price`, 
 (7, 'Beck', 'nullam molestie nibh in lectus pellentesque at', 'http://dummyimage.com/148x100.png/dddddd/000000', '13', '87', '4237e8dsx793', 12, 2, 1, 'lacinia eget tincidunt eget tempus vel pede morbi porttitor lorem id ligula suspendisse ornare', 3),
 (8, 'Ingunna', 'nulla ut erat id mauris vulputate elementum nullam varius', 'http://dummyimage.com/195x100.png/dddddd/000000', '93', '71', '4237e8dsx793', 47, 1, 0, 'ipsum primis in faucibus orci luctus et ultrices posuere cubilia', 3);
 
--- --------------------------------------------------------
+INSERT INTO `product_category` (`product_ID`, `category_ID`) VALUES
+(1, 1),
+(2, 1),
+(3, 2),
+(4, 3),
+(5, 4),
+(6, 5),
+(7, 4),
+(8, 3);
 
---
--- Struktura tabeli dla tabeli `product_category`
---
+INSERT INTO `product_filter` (`product_ID`, `filter_ID`) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 2),
+(5, 3),
+(6, 4),
+(7, 5),
+(8, 6);
 
-CREATE TABLE `product_category` (
-  `product_ID` int(11) NOT NULL,
-  `category_ID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `product_filter`
---
-
-CREATE TABLE `product_filter` (
-  `product_ID` int(11) NOT NULL,
-  `filter_ID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `product_flag`
---
-
-CREATE TABLE `product_flag` (
-  `product_ID` int(11) NOT NULL,
-  `flag_ID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `product_image`
---
-
-CREATE TABLE `product_image` (
-  `ID` int(11) NOT NULL,
-  `image_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `product_ID` int(11) DEFAULT NULL,
-  `main` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Zrzut danych tabeli `product_image`
---
+INSERT INTO `product_flag` (`product_ID`, `flag_ID`) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 2),
+(5, 3),
+(6, 4),
+(7, 5),
+(8, 6);
 
 INSERT INTO `product_image` (`ID`, `image_name`, `product_ID`, `main`) VALUES
 (1, 'http://dummyimage.com/113x100.png/ff4444/ffffff', 1, 0),
@@ -277,164 +117,13 @@ INSERT INTO `product_image` (`ID`, `image_name`, `product_ID`, `main`) VALUES
 (7, 'http://dummyimage.com/210x100.png/dddddd/000000', 6, 1),
 (8, 'http://dummyimage.com/243x100.png/cc0000/ffffff', 8, 0);
 
--- --------------------------------------------------------
+INSERT INTO `product_parameter_value` (`product_ID`, `parameter_value_ID`) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 2),
+(5, 3),
+(6, 4),
+(7, 5),
+(8, 6);
 
---
--- Struktura tabeli dla tabeli `product_parameter_value`
---
-
-CREATE TABLE `product_parameter_value` (
-  `product_ID` int(11) NOT NULL,
-  `parameter_value_ID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Indeksy dla zrzutów tabel
---
-
---
--- Indeksy dla tabeli `banner`
---
-ALTER TABLE `banner`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indeksy dla tabeli `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `parent` (`parent`);
-
---
--- Indeksy dla tabeli `delivery`
---
-ALTER TABLE `delivery`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indeksy dla tabeli `filter`
---
-ALTER TABLE `filter`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indeksy dla tabeli `filter_value`
---
-ALTER TABLE `filter_value`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `ID_filter` (`ID_filter`);
-
---
--- Indeksy dla tabeli `flag`
---
-ALTER TABLE `flag`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indeksy dla tabeli `parameter_value`
---
-ALTER TABLE `parameter_value`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indeksy dla tabeli `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `delivery_ID` (`delivery_ID`);
-
---
--- Indeksy dla tabeli `product_category`
---
-ALTER TABLE `product_category`
-  ADD PRIMARY KEY (`product_ID`,`category_ID`),
-  ADD KEY `category_ID` (`category_ID`);
-
---
--- Indeksy dla tabeli `product_filter`
---
-ALTER TABLE `product_filter`
-  ADD PRIMARY KEY (`product_ID`,`filter_ID`),
-  ADD KEY `filter_ID` (`filter_ID`);
-
---
--- Indeksy dla tabeli `product_flag`
---
-ALTER TABLE `product_flag`
-  ADD PRIMARY KEY (`product_ID`,`flag_ID`),
-  ADD KEY `flag_ID` (`flag_ID`);
-
---
--- Indeksy dla tabeli `product_image`
---
-ALTER TABLE `product_image`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `product_ID` (`product_ID`);
-
---
--- Indeksy dla tabeli `product_parameter_value`
---
-ALTER TABLE `product_parameter_value`
-  ADD PRIMARY KEY (`product_ID`,`parameter_value_ID`),
-  ADD KEY `parameter_value_ID` (`parameter_value_ID`);
-
---
--- Ograniczenia dla zrzutów tabel
---
-
---
--- Ograniczenia dla tabeli `category`
---
-ALTER TABLE `category`
-  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `category` (`ID`);
-
---
--- Ograniczenia dla tabeli `filter_value`
---
-ALTER TABLE `filter_value`
-  ADD CONSTRAINT `filter_value_ibfk_1` FOREIGN KEY (`ID_filter`) REFERENCES `filter` (`ID`);
-
---
--- Ograniczenia dla tabeli `product`
---
-ALTER TABLE `product`
-  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`delivery_ID`) REFERENCES `delivery` (`ID`);
-
---
--- Ograniczenia dla tabeli `product_category`
---
-ALTER TABLE `product_category`
-  ADD CONSTRAINT `product_category_ibfk_1` FOREIGN KEY (`product_ID`) REFERENCES `product` (`ID`),
-  ADD CONSTRAINT `product_category_ibfk_2` FOREIGN KEY (`category_ID`) REFERENCES `category` (`ID`);
-
---
--- Ograniczenia dla tabeli `product_filter`
---
-ALTER TABLE `product_filter`
-  ADD CONSTRAINT `product_filter_ibfk_1` FOREIGN KEY (`product_ID`) REFERENCES `product` (`ID`),
-  ADD CONSTRAINT `product_filter_ibfk_2` FOREIGN KEY (`filter_ID`) REFERENCES `filter` (`ID`);
-
---
--- Ograniczenia dla tabeli `product_flag`
---
-ALTER TABLE `product_flag`
-  ADD CONSTRAINT `product_flag_ibfk_1` FOREIGN KEY (`product_ID`) REFERENCES `product` (`ID`),
-  ADD CONSTRAINT `product_flag_ibfk_2` FOREIGN KEY (`flag_ID`) REFERENCES `flag` (`ID`);
-
---
--- Ograniczenia dla tabeli `product_image`
---
-ALTER TABLE `product_image`
-  ADD CONSTRAINT `product_image_ibfk_1` FOREIGN KEY (`product_ID`) REFERENCES `product` (`ID`);
-
---
--- Ograniczenia dla tabeli `product_parameter_value`
---
-ALTER TABLE `product_parameter_value`
-  ADD CONSTRAINT `product_parameter_value_ibfk_1` FOREIGN KEY (`product_ID`) REFERENCES `product` (`ID`),
-  ADD CONSTRAINT `product_parameter_value_ibfk_2` FOREIGN KEY (`parameter_value_ID`) REFERENCES `parameter_value` (`ID`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
