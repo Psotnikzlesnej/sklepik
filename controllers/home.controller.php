c67699a9caaa6798ea92fd0a33d231862e7ca8aa
canicjusz <janek.bananek371@gmail.com>
2023-05-11 08:46:55 +0200

    funkcja do przetwarzania bannerow i ulepszenie tej od polecanch produktow #18

--- controllers/home.controller.php
+++ controllers/home.controller.php
@@ -1,64 +1,86 @@
 <?php
 include_once(__DIR__ . '/../models/home.model.php');
 
-$mashed_products = [];
-$products = [];
-
-function compute_featured(){
-  global $mashed_products;
-  global $products;
-  $result = get_buyable();
-  while ($array = $result->fetch_assoc()) {
-    array_push($products, $array);
+function process_featured(){
+  $featured_ID_array = [];
+  $featured_ID_result = get_featured_ID();
+  while ($featured_ID_row = $featured_ID_result->fetch_assoc()) {
+    $featured_ID = $featured_ID_row['ID'];
+    array_push($featured_ID_array, $featured_ID);
   }
-  $last_mashed_arr = array_reduce($products, "masher");
-  array_push($mashed_products, $last_mashed_arr);
-  $featured = array_filter($mashed_products, "featured");
-  foreach($featured as $product){
-    print_r($product);
-      echo "<br>";
-  };
+
+  $products = [];
+  $featured_products_result = get_certain_products($featured_ID_array);
+  while ($featured_product = $featured_products_result->fetch_assoc()) {
+    array_push($products, $featured_product);
+  }
+
+  $masher = new ProductMasher($products);
+  $mashed_products = $masher->mashWrapper();
+
+  return $mashed_products;
 }
 
-function create_mashed_arr($attrs){
-  return [
-    'ID' => $attrs['ID'],
-    'name' => $attrs['name'],
-    'promo_price' => $attrs['promo_price'],
-    'catalog_price' => $attrs['catalog_price'],
-    'serial_number' => $attrs['serial_number'],
-    'stock' => $attrs['stock'],
-    'flags' => [$attrs['flag_name']],
-    'image_name' => $attrs['image_name']
-  ];
+class ProductMasher {
+
+  function __construct($arr){
+    $this->initial_arr = $arr;
+    $this->mashed_arr = [];
+  }
+
+  public function mashWrapper(){
+    $last_mashed_arr = array_reduce($this->initial_arr, [$this, 'mash']);
+    array_push($this->mashed_arr, $last_mashed_arr);
+    return $this->mashed_arr;
+  }
+
+  private function createMashedArr($attrs){
+    return [
+      'ID' => $attrs['ID'],
+      'name' => $attrs['name'],
+      'promo_price' => $attrs['promo_price'],
+      'catalog_price' => $attrs['catalog_price'],
+      'serial_number' => $attrs['serial_number'],
+      'stock' => $attrs['stock'],
+      'flags' => [$attrs['flag_name']],
+      'image_name' => $attrs['image_name']
+    ];
+  }
+
+  private function mash($mashed_arr, $product_arr){
+    $is_different_product = (isset($mashed_arr) && ($mashed_arr['ID'] != $product_arr['ID']));
+  
+    if($is_different_product){
+      array_push($this->mashed_arr, $mashed_arr);
+    }
+    if(!isset($mashed_arr) || $is_different_product){
+      $new_mashed_arr = $this->createMashedArr($product_arr);
+      return $new_mashed_arr;
+    }
+  
+    if($product_arr['main']){
+      $mashed_arr['main'] = true;
+      $mashed_arr['image_name'] = $product_arr['image_name'];
+    }
+  
+    $is_new_flag = !in_array($product_arr['flag_name'], $mashed_arr['flags']);
+    if($is_new_flag){
+      array_push($mashed_arr['flags'], $product_arr['flag_name']);
+    }
+  
+    return $mashed_arr;
+  }
+
 }
 
-// prosze nie czytac, grozi zawalem!
-function masher($mashed_arr, $product_arr){
-  global $mashed_products;
-  $is_different_product = (isset($mashed_arr) && ($mashed_arr['ID'] != $product_arr['ID']));
-  if($is_different_product){
-    array_push($mashed_products, $mashed_arr);
+function process_banners(){
+  $banners_sorted = ['home_top' => [], 'home_tile'  => [], 'home_bottom'  => []];
+  $banners = get_home_banners();
+  while ($banner = $banners->fetch_assoc()) {
+    $type = $banner['type'];
+    array_push($banners_sorted[$type], $banner);
   }
-  if(!isset($mashed_arr) || $is_different_product){
-    $new_mashed_arr = create_mashed_arr($product_arr);
-    return $new_mashed_arr;
-  }
-  if($product_arr['main']){
-    $mashed_arr['main'] = true;
-    $mashed_arr['image_name'] = $image_name;
-  }
-  $is_new_flag = !in_array($product_arr['flag_name'], $mashed_arr['flags']);
-  if($is_new_flag){
-    array_push($mashed_arr['flags'], $product_arr['flag_name']);
-  }
-  return $mashed_arr;
+  return $banners_sorted;
 }
-
-function featured($arr){
-  return in_array('featured', $arr['flags']);
-}
-
-compute_featured();
 
 ?>
--- models/home.model.php
+++ models/home.model.php
@@ -1,36 +1,44 @@
 <?php
 
-function get_top_banners(){
+function get_home_banners(){
   global $mysqli;
-  $query = "SELECT * FROM `banner` WHERE visible=true AND type=home_top";
+  $query = "SELECT title, description, image_name, link, type, alt, mask FROM `banner` 
+  WHERE visible=true AND type IN ('home_top', 'home_tile', 'home_bottom')";
   $result = $mysqli->query($query);
   return $result;
 }
 
-function get_tiles(){
+// function get_buyable(){
+//   global $mysqli;
+//   $query = "SELECT p.ID, p.name, p.promo_price, p.catalog_price, p.serial_number,
+//   p.stock, f.name as flag_name, p_i.main, p_i.image_name FROM `product` as p
+// 	  JOIN `product_flag` as p_f on p_f.product_ID = p.ID
+//     JOIN `flag` as f on p_f.flag_ID = f.ID
+//     LEFT JOIN `product_image` as p_i on p_i.product_ID = p.ID
+//       WHERE p.visible = true AND p.stock > 0 ORDER BY p.ID;";
+//   $result = $mysqli->query($query);
+//   return $result;
+// }
+
+function get_featured_ID(){
   global $mysqli;
-  $query = "SELECT * FROM `banner` WHERE visible=true AND type=home_tile";
+  $query = "SELECT p.ID FROM `product` as p
+    JOIN `product_flag` as p_f on p_f.product_ID = p.ID
+    JOIN `flag` as f on p_f.flag_ID = f.ID
+      WHERE p.visible = true AND p.stock > 0 AND f.name = 'featured';";
   $result = $mysqli->query($query);
   return $result;
 }
 
-function get_bottom_banner(){
-  global $mysqli;
-  $query = "SELECT * FROM `banner` WHERE visible=true AND type=home_bottom";
-  $result = $mysqli->query($query);
-  return $result;
-}
-
-function get_buyable(){
+function get_certain_products($ids){
   global $mysqli;
   $query = "SELECT p.ID, p.name, p.promo_price, p.catalog_price, p.serial_number,
   p.stock, f.name as flag_name, p_i.main, p_i.image_name FROM `product` as p
 	  JOIN `product_flag` as p_f on p_f.product_ID = p.ID
     JOIN `flag` as f on p_f.flag_ID = f.ID
     LEFT JOIN `product_image` as p_i on p_i.product_ID = p.ID
-      WHERE p.visible = true AND p.stock > 0 ORDER BY p.ID;";
+      WHERE p.visible = true AND p.stock > 0 AND p.ID IN (" . implode(',', $ids) . ") ORDER BY p.ID;";
   $result = $mysqli->query($query);
   return $result;
 }
-
 ?>
