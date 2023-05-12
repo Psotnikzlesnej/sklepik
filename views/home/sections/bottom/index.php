766de61052e8f78b038c58e9027f8f7396aa9de5
Nikrop <ltomasz989@gmail.com>
2023-05-12 07:37:12 +0200

    #15

---
+++ views/home/sections/bottom/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="whyUs">
+        <img src="" alt="" class="whyUs__img">
+        <div class="whyUs__content">
+            <h3 class="whyUs__heading">Sprawdź dlaczego my</h3>
+            <div class="whyUs__line">
+                <hr class="whyUs__lineColorRed">
+                <hr class="whyUs__lineColorWhite">
+            </div>
+            <p class="whyUs__paragraph">
+                Początki działalności to rok 2002r i otwarcie hurtowni elektrycznej,
+                oferującej podstawowe produkty do instalacji elektrycznych jak i również
+                materiały używane w przemyśle wydobywczym.
+                Firma dostarczała produkty trudno dostępne do kopalń,
+                zakładów przemysłowych.
+            </p>
+            <ul class="whyUs__list">
+                <li class="whyUs__listItem">
+                    <img class="whyUs__listItemIcon" src="icon1.png" alt="">
+                    <span class="whyUs__listItemSpan">szybka wysyłka</span>
+                </li>
+                <li class="whyUs__listItem">
+                    <img class="whyUs__listItemIcon" src="icon2.png" alt="">
+                    <span class="whyUs__listItemSpan">szeroki asortyment</span>
+                </li>
+                <li class="whyUs__listItem">
+                    <img class="whyUs__listItemIcon" src="icon3.png" alt="">
+                    <span class="whyUs__listItemSpan">bezpieczne zwroty</span>
+                </li>
+            </ul>
+            <button class="whyUs__btn">
+                <span class="whyUs__btnSpan">
+                    <a href="#" class="whyUs__btnSpanAnchor">infromacje o firmie</a>
+                </span>
+            </button>
+        </div>
+        <div class="whyUs__bg"></div>
+    </section>
+</body>
+</html>
