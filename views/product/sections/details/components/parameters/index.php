da9c55d686a9285374a6b4dbef5a08e73e284a53
jacusdestroyer <drawskojacek@wp.pl>
2023-05-12 14:36:46 +0200

    #21 dolna czesc strony produktu

---
+++ views/product/sections/details/components/parameters/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Details</title>
+</head>
+<body>
+  
+
+<div class="parameters_layout">
+
+</div>
+
+   <div class="parameters_box">
+
+ </div>   
+      <div class="parameters_box_text_top">DANE PODSTAWOWE </div>
+
+  
+
+ 
+      <div class="parameters_box_text1"> </div>
+      <div class="parameters_box_text2"> </div>
+      
+      <span class="parameters_box_text_span1">adaed</span> 
+      <span class="parameters_box_text_span2">adaed</span> 
+      <span class="parameters_box_text_span3">adaed</span> 
+      <span class="parameters_box_text_span4">adaed</span> 
+      <span class="parameters_box_text_span5">adaed</span> 
+      <span class="parameters_box_text_span6">adaed</span> 
+      <span class="parameters_box_text_span7">adaed</span> 
+
+
+
+
+
+
+
+
+
+         <div class="parameters_box_detail_white_1">   </div>
+         <div class="parameters_box_detail_blue_1">  </div>
+         <div class="parameters_box_detail_white_2">    </div>
+         <div class="parameters_box_detail_blue_2"> </div>
+         <div class="parameters_box_detail_white_3"> </div>
+         <div class="parameters_box_detail_blue_3">    </div>
+         <div class="parameters_box_detail_white_4">    </div>
+         <div class="parameters_box_detail_blue_4">   </div>
+ 
+
+        
+
+
+
+
+
+
+
+
+</body>
+</html>
