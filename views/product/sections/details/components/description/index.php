a530bf6adada018c667f1c18bcb7454fe6e18618
jacusdestroyer <drawskojacek@wp.pl>
2023-05-15 10:51:47 +0200

    #21 sekcja details w stronie produktu gotowa

---
+++ views/product/sections/details/components/description/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../../details/style.css">
+
+    <title>Details</title>
+</head>
+<body>
+  
+
+<div class="description_layout"></div>
+
+   <div class="description_box"></div>  
+
+      <div class="parameters_box_text_top_description">DANE PODSTAWOWE </div>
+
+      <div class="description_box_text_line"></div>
+
+
+
+      <div class="description_box_text">Simon 10 to ekonomiczna seria modułowa dla inwestycji,
+         na których zróżnicowana kolorystyka nie jest wymagana. Dwa podstawowe kolory: biały i kremowy oraz
+          pełen przekrój asortymentu łączników i gniazd pozwolą na kompleksowe wyposażenie każdego mieszkania lub biura.
+           Elementem wyróżniającym tę serię jest nieprzewodzący prądu mostek wykonany z 
+        wysokiej jakości tworzywa zapewniającego trwałość i elastyczność oraz bezpieczeństwo podczas prac montażowych</div>
+
+        <div class="description_box_text2">       <span class="description_span_big_top">GNIAZDKA I ŁĄCZNIKI KONTAKT SIMON 10</span>
+
+
+        <span class="description_span1">  Dron Mavic Air 2 oferuje wyższy poziom mocy i mobilności – tak zaawansowane funkcje 
+            jeszcze nigdy nie były dostępne w tak kompaktowej konstrukcji. Inteligentne funkcje nagrywania i znakomita jakość obrazu sprawiają, że teraz każdy może 
+            tworzyć podniebne arcydzieła. A ponieważ lot przebiega bezpiecznie i sprawnie, masz jeszcze większe możliwości i możesz w pełni oddać się realizacji najśmielszych 
+            pomysłów. </span>
+
+            <span class="description_span2"> 
+            Czterostopniowe zatrzaski na każdej ramce zapewniają równe przyleganie ramek nawet do nierównych ścian. Również zamontowanie w puszkach do ścian 
+            karton-gips nie powoduje odstawiania osprzętu Simon 10 od ściany. Każdy moduł łącznika / przycisku przygotowany jest do wersji IP44, wystarczy dołożyć uszczelkę.</span>
+            
+            
+          <span class="description_span_big_down">  MOŻLIWOŚĆ ZDJĘCIA RAMKI BEZ DEMONTAŻU POKRYWY LUB KLAWISZA </span>
+
+
+          <span class="description_span_down">       Seria Simon 10 dedykowana jest inwestycjom, gdzie wytworny design i różnorodność w kolorach osprzętu nie jest najważniejsza. 
+              W tej linii produktów liczy się przede wszystkim ekonomiczność, funkcjonalność oraz trwałość. Uniwersalne kolory: biały i kremowy pozwolą 
+              na wykorzystanie łączników oraz gniazd w każdym mieszkaniu czy też przestrzeni biurowej.</span></div>
+
+<img src="test.png" class="test"></img>
+
+ 
+    
+
+        
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ views/product/sections/details/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+     <link rel="stylesheet" href="../details/components/description/style.css">
+     <link rel="stylesheet" href="../details/components/parameters/style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php 
+
+include '../details/components/description/index.php';
+
+include '../details/components/parameters/index.php';
+
+
+
+
+?>
+
+
+</body>
+</html>
