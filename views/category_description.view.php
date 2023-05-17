05ac2a08c4be4ce5fad769567e24cf69fa49ea19
jacusdestroyer <drawskojacek@wp.pl>
2023-05-17 12:00:15 +0200

    #1 zmiany nazw z controler na model

--- views/category_description.view.php
+++ views/category_description.view.php
@@ -4,17 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/category_description.css">
     <link rel="stylesheet" href="style.scss">
-    <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
     <div class="info_container">
 
             <div class= "container">
-            <h1>Przykładowy tytuł<h1>
+            <h1>Przykładowy tytuł</h1>
 
-<p>no
+
+<div class="lapa" id="lapa">
+
+<p>Gniazda elektryczne są niezbędnym elementem instalacji elektrycznej. To dzięki nim użytkownicy mogą zasilić urządzenia i sprzęt. Kontakty elektryczne są funkcjonalne i łatwe w montażu. Posiadamy zarówno gniazda instalacyjne natynkowe, podtynkowe, nowoczesne gniazdka teleinformatyczne, RTV i SAT, jak i hermetyczne, sprawdzające się tam, gdzie istnieje ryzyko kontaktu z wodą – łazienka, kuchnia
         <span id="dots">...</span>
     <div id="more">
 
@@ -24,10 +27,11 @@
     </div>
 </p>
 
+</div>
+
      <button id="btn"  onclick="readmore()">Rozwiń</button>
             </div>
             </div>
-
 
             <script>
 
@@ -36,13 +40,17 @@ function readmore()
 var dots = document.getElementById("dots");
 var more = document.getElementById("more");
 var btn = document.getElementById("btn");
+var lapa = document.getElementById("lapa");
 
 if (dots.style.display === "none")
 {
     dots.style.display = "inline";
     btn.innerHTML = "Rozwiń";
     more.style.display = 'none';
+    lapa.style.height = '50px';
+
 }else{
+    lapa.style.height = '150px';
     dots.style.display = 'none';
     btn.innerHTML = "Zwiń";
     more.style.display = 'inline';
