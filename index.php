376bcfee094c6cec1ef8f7907d3fac785f0846e1
canicjusz <janek.bananek371@gmail.com>
2023-05-17 11:24:46 +0200

    #1 nowa struktura i router

--- index.php
+++ index.php
@@ -2,21 +2,32 @@
 include_once(__DIR__ . '/utils/php/error_handling.php'); 
 include_once(__DIR__ . '/utils/php/config.php'); 
 include_once(__DIR__ . '/utils/php/debug.php'); 
-$url = isset($_GET['url']) ? $_GET['url'] : 'index.php';
+$url = isset($_GET['url']) ? $_GET['url'] : 'home';
 $splitted_url = explode("/", $url);
 $layout_determiner = $splitted_url[0];
-switch($layout_determiner){
-  case 'index.php':
-    include_once(__DIR__ . '/views/home/index.php');
-    break;
-  case 'catalog':
-    include_once(__DIR__ . '/views/product_list/index.php');
-    break;
-  case 'product':
-    include_once(__DIR__ . '/views/product/index.php');
-    break;
-  default:
-    include_once(__DIR__ . '/views/not_found/index.php');
+
+if(isset($splitted_url[1])){
+  $id = $splitted_url[1];
+  $GLOBALS[$layout_determiner . '_id'] = $id;
+}
+
+$query_string = explode('?', $_SERVER['REQUEST_URI'])[1] ?? '';
+$pairs = explode('&', $query_string);
+
+foreach($pairs as $pair){
+  $splitted_pair = explode('=', $pair);
+  $key = $splitted_pair[0];
+  $value = $splitted_pair[1] ?? '';
+  $GLOBALS['query_'.$key] = $value;
+}
+
+$controller_path = __DIR__ . '/controllers/' . $layout_determiner . '.controller.php';
+$not_found_path = __DIR__ . '/controllers/not_found.controller.php';
+if(file_exists($controller_path)){
+  include_once($controller_path);
+}else{
+  echo "xD";
+  // include_once($not_found_path);
 }
 
 ?>
