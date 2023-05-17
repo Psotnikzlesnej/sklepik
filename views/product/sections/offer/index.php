7564be81f5747384dab9ff999a10818d0d1bfd7d
mikolajczake <emilcancel@gmail.com>
2023-05-17 08:53:08 +0200

    #32 product offer

---
+++ views/product/sections/offer/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.scss">
+  
+  <title>Document</title>
+</head>
+<body>
+<div class="main-text">Łącznik jednobiegunowy (moduł) 10AX, 250V~, zaciski śrubowe
+<img src="Philips_logo.svg.png" style="height: 17px; "alt="">
+</div>
+
+  <div class="container">
+    
+   
+<div class="shop-min">
+<div class="id">IPC-HFW2231S-S-0280B<hr style="border:  1px solid #E1E2E8; margin-top: 15px;"><div>
+<div class="main_id">
+  
+<div class="Dostępne" > <svg xmlns="http://www.w3.org/2000/svg" width="17.23" height="17.353" viewBox="0 0 17.23 17.353"style="margin-right:10px; ">
+  <g id="Group_5248" data-name="Group 5248" transform="translate(0.5 0.5)">
+    <path id="Path_4441" data-name="Path 4441" d="M25.23,13.426l-8.115,4.426L9,13.426,17.115,9Z" transform="translate(-9 -9)" fill="none" stroke="#80b61d" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+    <path id="Path_4442" data-name="Path 4442" d="M25.23,63v7.377L17.115,74.8,9,70.377V63" transform="translate(-9 -58.574)" fill="none" stroke="#80b61d" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+    <line id="Line_379" data-name="Line 379" y2="7.323" transform="translate(8.115 9.03)" fill="none" stroke="#80b61d" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+    <line id="Line_380" data-name="Line 380" x2="8.115" y2="4.426" transform="translate(4.057 2.213)" fill="none" stroke="#80b61d" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+  </g>
+  
+</svg>Dostępny</div>
+<hr style="margin:0;  border: 1px solid #E1E2E8;     height: 33px;" ><div class="xd"><svg xmlns="http://www.w3.org/2000/svg" width="19.06" height="13.956" viewBox="0 0 19.06 13.956" style="margin-right:10px;">
+  <g id="Group_5250" data-name="Group 5250" transform="translate(0.5 0.5)">
+    <g id="Group_5251" data-name="Group 5251" transform="translate(0 0)">
+      <circle id="Ellipse_164" data-name="Ellipse 164" cx="1.5" cy="1.5" r="1.5" transform="translate(2.179 9.956)" fill="none" stroke="#1d1d1b" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+      <circle id="Ellipse_165" data-name="Ellipse 165" cx="1.5" cy="1.5" r="1.5" transform="translate(13.179 9.956)" fill="none" stroke="#1d1d1b" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+      <line id="Line_381" data-name="Line 381" x2="8" transform="translate(5.179 11.956)" fill="none" stroke="#1d1d1b" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+      <path id="Path_4443" data-name="Path 4443" d="M20.493,16.388V9H9V20.493h2.463" transform="translate(-9 -9)" fill="none" stroke="#1d1d1b" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+      <path id="Path_4444" data-name="Path 4444" d="M135,27h1.642a4.923,4.923,0,0,1,4.925,4.925v4.925h-1.642" transform="translate(-123.507 -25.358)" fill="none" stroke="#1d1d1b" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+    </g>
+  </g>
+</svg>24H</div>
+
+</div><br>
+<div class="product"><h5>WARIANTY<h5></div>
+
+</div>
+<div class="container-product">
+
+<div class="item1">
+<img src="Image 149.png" alt="light" style="background: #FFFFFF 0% 0% no-repeat padding-box;
+border: 1px solid #E1E2E8;
+opacity: 1;     padding: 6px 14px;" >
+<div style="
+    display: flex;
+    align-items: center;
+    justify-content: center;
+">Biały</div>
+</div>
+<div class="item2">
+<img src="Image 149.png" alt="light" style="background: #FFFFFF 0% 0% no-repeat padding-box;
+border: 1px solid #E1E2E8;
+opacity: 1;     padding: 6px 14px; ">
+
+<div style="
+    display: flex;
+    align-items: center;
+    justify-content: center;
+">Czarny</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="shopme">
+<div class="high"><s>656,65 zł</s></div>
+<div class="low">442,85 zł</div>
+</div>
+
+</body>
+</html>
