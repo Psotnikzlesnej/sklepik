cc7024d8e46d7c2280204e6ffbe033fcba012d33
canicjusz <janek.bananek371@gmail.com>
2023-05-18 15:29:56 +0200

    #39 podstawowa funkcjonalnosc zaimplementowana

--- components/select.component.php
+++ components/select.component.php
@@ -1,15 +1,17 @@
 <script src="../js/select.js" defer></script>
 <link rel="stylesheet" href="../styles/counter.css">
 <template id="select">
-  <span class="select__current-value"></span>
+  <span class="select__current-element"></span>
   <span class="select__icon"></span>
   <ul class="select__list">
     <slot name="item"></slot>
   </ul>
 </template>
-<custom-select data-default-value="1">
+<custom-select>
   <li slot="item" class="select__list-item" data-value="1">x</li>
   <li slot="item" class="select__list-item" data-value="2">d</li>
+  <li slot="item" class="select__list-item" data-value="2">b</li>
+  <li slot="item" class="select__list-item" data-value="2">k</li>
 </custom-select>
 
   <!-- <span class="select__current-value"></span>
--- js/select.js
+++ js/select.js
@@ -1,20 +1,31 @@
 class SelectElement extends HTMLElement {
   constructor() {
     super();
-    this.selectTemplate = document.querySelector('#select')
-    this.attachShadow({mode: 'open'})
+    this.selectTemplate = document.querySelector("#select");
+    this.attachShadow({ mode: "open" });
     const clone = this.selectTemplate.content.cloneNode(true);
     this.shadowRoot.appendChild(clone);
-    this.currentValue = this.querySelector('.select__current-value')
-    const slot = this.shadowRoot.querySelector('slot')
-    this.list = slot.assignedNodes()
-    this.value = this.getAttribute('data-default-value')
-    this.curr_item = ''
-    this.list.forEach(el=>{
-      el.addEventListener('click', e=>{
-        this.value
-      })
-    })
+    this.currentElement = this.shadowRoot.querySelector(
+      ".select__current-element"
+    );
+    const slot = this.shadowRoot.querySelector("slot");
+    this.list = [...slot.assignedElements()];
+    const defaultValue = this.getAttribute("data-default-value");
+    this.value = defaultValue;
+    const selectedByDefault = this.list.find((el) =>
+      defaultValue ? el.getAttribute("data-value") === defaultValue : true
+    );
+    this.setNewContent(selectedByDefault);
+    this.list.forEach((el) => {
+      el.addEventListener("click", (_) => {
+        this.value = el.getAttribute("data-value");
+        this.setNewContent(el);
+      });
+    });
+  }
+
+  setNewContent(element) {
+    this.currentElement.textContent = element.textContent;
   }
 }
-customElements.define('custom-select', SelectElement)
+customElements.define("custom-select", SelectElement);
