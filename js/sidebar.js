5a58415ac6d786c6cc7e907b473d3318ee5ef992
Nikrop <ltomasz989@gmail.com>
2023-05-18 09:09:09 +0200

    #27 dodanie sidebara

---
+++ js/sidebar.js
@@ -0,0 +1,94 @@
+function toggleElementVisibility1() {
+    let x = document.getElementById("sidebar__showHide1");
+    if (x.style.display === "block") {
+        document.getElementById("changeIcon1").classList.add('plus');
+        document.getElementById("changeIcon1").classList.remove('minus');
+        if ( document.getElementById("changeIcon1").classList.contains('minus') )
+        document.getElementById("changeIcon1").classList.toggle('plus');
+
+        x.style.display = "none";
+    } else {
+        document.getElementById("changeIcon1").classList.add('minus');
+        document.getElementById("changeIcon1").classList.remove('plus');
+        if ( document.getElementById("changeIcon1").classList.contains('plus') )
+        document.getElementById("changeIcon1").classList.toggle('minus');
+
+        x.style.display = "block";
+    }
+}
+
+function toggleElementVisibility2() {
+    let x = document.getElementById("sidebar__showHide2");
+    if (x.style.display === "block") {
+        document.getElementById("changeIcon2").classList.add('plus');
+        document.getElementById("changeIcon2").classList.remove('minus');
+        if ( document.getElementById("changeIcon2").classList.contains('minus') )
+        document.getElementById("changeIcon2").classList.toggle('plus');
+
+        x.style.display = "none";
+    } else {
+        document.getElementById("changeIcon2").classList.add('minus');
+        document.getElementById("changeIcon2").classList.remove('plus');
+        if ( document.getElementById("changeIcon2").classList.contains('plus') )
+        document.getElementById("changeIcon2").classList.toggle('minus');
+
+        x.style.display = "block";
+    }
+}
+
+function toggleElementVisibility3() {
+    let x = document.getElementById("sidebar__showHide3");
+    if (x.style.display === "block") {
+        document.getElementById("changeIcon3").classList.add('plus');
+        document.getElementById("changeIcon3").classList.remove('minus');
+        if ( document.getElementById("changeIcon3").classList.contains('minus') )
+        document.getElementById("changeIcon3").classList.toggle('plus');
+
+        x.style.display = "none";
+    } else {
+        document.getElementById("changeIcon3").classList.add('minus');
+        document.getElementById("changeIcon3").classList.remove('plus');
+        if ( document.getElementById("changeIcon3").classList.contains('plus') )
+        document.getElementById("changeIcon3").classList.toggle('minus');
+
+        x.style.display = "block";
+    }
+}
+
+function toggleElementVisibility4() {
+    let x = document.getElementById("sidebar__showHide4");
+    if (x.style.display === "block") {
+        document.getElementById("changeIcon4").classList.add('plus');
+        document.getElementById("changeIcon4").classList.remove('minus');
+        if ( document.getElementById("changeIcon4").classList.contains('minus') )
+        document.getElementById("changeIcon4").classList.toggle('plus');
+
+        x.style.display = "none";
+    } else {
+        document.getElementById("changeIcon4").classList.add('minus');
+        document.getElementById("changeIcon4").classList.remove('plus');
+        if ( document.getElementById("changeIcon4").classList.contains('plus') )
+        document.getElementById("changeIcon4").classList.toggle('minus');
+
+        x.style.display = "block";
+    }
+}
+
+function toggleElementVisibility5() {
+    let x = document.getElementById("sidebar__showHide5");
+    if (x.style.display === "block") {
+        document.getElementById("changeIcon5").classList.add('plus');
+        document.getElementById("changeIcon5").classList.remove('minus');
+        if ( document.getElementById("changeIcon5").classList.contains('minus') )
+        document.getElementById("changeIcon5").classList.toggle('plus');
+
+        x.style.display = "none";
+    } else {
+        document.getElementById("changeIcon5").classList.add('minus');
+        document.getElementById("changeIcon5").classList.remove('plus');
+        if ( document.getElementById("changeIcon5").classList.contains('plus') )
+        document.getElementById("changeIcon5").classList.toggle('minus');
+
+        x.style.display = "block";
+    }
+}
---
+++ views/sidebar.view.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/sidebar.css">
+    <script src="../js/sidebar.js"></script>
+</head>
+<body>
+    <aside class="sidebar">
+        <span class="sidebar__heading">Gniazdka, wyłączniki</span>
+        <hr class="sidebar__hr">
+        <ul class="sidebar__list">
+            <li class="sidebar__listItem">
+                <a class="sidebar__listItemAnchor" href="#">
+                    <span class="sidebar__listItemSpan">
+                        SCHNEIDER ELECTRIC
+                    </span>
+                </a>
+                <a class="sidebar__btn icon" href="javascript:void(0);" onclick="toggleElementVisibility1()">
+                    <i class="sidebar__btnIcon plus" id="changeIcon1"></i>
+                </a>
+            </li>
+            <ul class="sidebar__nestedList" id="sidebar__showHide1">
+                <li class="sidebar__nestedListItem">
+                    <a class="sidebar__nestedListItemAnchor" href="#">
+                        <span class="sidebar__nestedListItemSpan">
+                            test
+                        </span>
+                    </a>
+                </li>
+                <li class="sidebar__nestedListItem">
+                    <a class="sidebar__nestedListItemAnchor" href="#">
+                        <span class="sidebar__nestedListItemSpan">
+                            test
+                        </span>
+                    </a>
+                </li>
+            </ul>
+            <li class="sidebar__listItem">
+                <a class="sidebar__listItemAnchor" href="#">
+                    <span class="sidebar__listItemSpan">
+                        Kontakt simon
+                    </span>
+                </a>
+                <a class="sidebar__btn icon" href="javascript:void(0);" onclick="toggleElementVisibility2()">
+                    <i class="sidebar__btnIcon plus" id="changeIcon2"></i>
+                </a>
+            </li>
+            <ul class="sidebar__nestedList" id="sidebar__showHide2">
+                <li class="sidebar__nestedListItem">
+                    <a class="sidebar__nestedListItemAnchor" href="#">
+                        <span class="sidebar__nestedListItemSpan">
+                            test
+                        </span>
+                    </a>
+                </li>
+                <li class="sidebar__nestedListItem">
+                    <a class="sidebar__nestedListItemAnchor" href="#">
+                        <span class="sidebar__nestedListItemSpan">
+                            test
+                        </span>
+                    </a>
+                </li>
+            </ul>
+            <li class="sidebar__listItem">
+                <a class="sidebar__listItemAnchor" href="#">
+                    <span class="sidebar__listItemSpan">
+                        Berker
+                    </span>
+                </a>
+                <a class="sidebar__btn icon" href="javascript:void(0);" onclick="toggleElementVisibility3()">
+                    <i class="sidebar__btnIcon plus" id="changeIcon3"></i>
+                </a>
+            </li>
+            <ul class="sidebar__nestedList" id="sidebar__showHide3">
+                <li class="sidebar__nestedListItem">
+                    <a class="sidebar__nestedListItemAnchor" href="#">
+                        <span class="sidebar__nestedListItemSpan">
+                            test
+                        </span>
+                    </a>
+                </li>
+                <li class="sidebar__nestedListItem">
+                    <a class="sidebar__nestedListItemAnchor" href="#">
+                        <span class="sidebar__nestedListItemSpan">
+                            test
+                        </span>
+                    </a>
+                </li>
+            </ul>
+            <li class="sidebar__listItem">
+                <a class="sidebar__listItemAnchor" href="#">
+                    <span class="sidebar__listItemSpan">
+                        Karlik
+                    </span>
+                </a>
+                <a class="sidebar__btn icon" href="javascript:void(0);" onclick="toggleElementVisibility4()">
+                    <i class="sidebar__btnIcon plus" id="changeIcon4"></i>
+                </a>
+            </li>
+            <ul class="sidebar__nestedList" id="sidebar__showHide4">
+                <li class="sidebar__nestedListItem">
+                    <a class="sidebar__nestedListItemAnchor" href="#">
+                        <span class="sidebar__nestedListItemSpan">
+                            test
+                        </span>
+                    </a>
+                </li>
+                <li class="sidebar__nestedListItem">
+                    <a class="sidebar__nestedListItemAnchor" href="#">
+                        <span class="sidebar__nestedListItemSpan">
+                            test
+                        </span>
+                    </a>
+                </li>
+            </ul>
+            <li class="sidebar__listItem">
+                <a class="sidebar__listItemAnchor" href="#">
+                    <span class="sidebar__listItemSpan">
+                        Hager
+                    </span>
+                </a>
+                <a class="sidebar__btn icon" href="javascript:void(0);" onclick="toggleElementVisibility5()">
+                    <i class="sidebar__btnIcon plus" id="changeIcon5"></i>
+                </a>
+            </li>
+            <ul class="sidebar__nestedList" id="sidebar__showHide5">
+                <li class="sidebar__nestedListItem">
+                    <a class="sidebar__nestedListItemAnchor" href="#">
+                        <span class="sidebar__nestedListItemSpan">
+                            test
+                        </span>
+                    </a>
+                </li>
+                <li class="sidebar__nestedListItem">
+                    <a class="sidebar__nestedListItemAnchor" href="#">
+                        <span class="sidebar__nestedListItemSpan">
+                            test
+                        </span>
+                    </a>
+                </li>
+            </ul>
+            <li class="sidebar__listItem">
+                <a class="sidebar__listItemAnchor" href="#">
+                    <span class="sidebar__listItemSpan">
+                        Ospel
+                    </span>
+                </a>
+            </li>
+            <li class="sidebar__listItem">
+                <a class="sidebar__listItemAnchor" href="#">
+                    <span class="sidebar__listItemSpan">
+                        Kos
+                    </span>
+                </a>
+            </li>
+            <li class="sidebar__listItem">
+                <a class="sidebar__listItemAnchor" href="#">
+                    <span class="sidebar__listItemSpan">
+                        Viplast
+                    </span>
+                </a>
+            </li>
+        </ul>
+    </aside>
+</body>
+</html>
