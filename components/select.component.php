f99319e86cba7cfcec448501f8e312859275b285
canicjusz <janek.bananek371@gmail.com>
2023-05-19 10:55:06 +0200

    #39 custom select zrobiony

--- components/select.component.php
+++ components/select.component.php
@@ -1,23 +1,17 @@
 <script src="../js/select.js" defer></script>
-<link rel="stylesheet" href="../styles/counter.css">
 <template id="select">
-  <span class="select__current-element"></span>
-  <span class="select__icon"></span>
-  <ul class="select__list">
+<link rel="stylesheet" href="../styles/select.css">
+  <div class="select__current-element">
+    <span class="select__current-content"></span>
+    <span class="select__icon"></span>
+  </div>
+  <ul class="select__list select__list--hidden">
     <slot name="item"></slot>
   </ul>
 </template>
-<custom-select>
+<!-- <custom-select class="select">
   <li slot="item" class="select__list-item" data-value="1">x</li>
   <li slot="item" class="select__list-item" data-value="2">d</li>
   <li slot="item" class="select__list-item" data-value="2">b</li>
   <li slot="item" class="select__list-item" data-value="2">k</li>
-</custom-select>
-
-  <!-- <span class="select__current-value"></span>
-  <span class="select__icon"></span>
-  <ul class="select__list">
-    <slot name="item1"></slot>
-    <li slot="item1">x</li>
-  <li slot="item1">d</li>
-  </ul> -->
+</custom-select> -->
