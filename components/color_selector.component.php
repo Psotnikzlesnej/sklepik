c2d236f9a26b736421d27e6b6d50f2fb48536ff0
canicjusz <janek.bananek371@gmail.com>
2023-05-22 11:26:36 +0200

    #44 color picker zrobiony

---
+++ components/color_selector.component.php
@@ -0,0 +1,17 @@
+<script src="../js/color_selector.js" defer></script>
+<template id="color-selector">
+<link rel="stylesheet" href="../styles/color_selector.css">
+  <ul class="color-selector__list color-selector__list--hidden">
+    <slot name="item"></slot>
+  </ul>
+</template>
+<color-selector class="color-selector" data-selected="1">
+  <li slot="item" class="color-selector__list-item" data-value="1">black</li>
+  <li slot="item" class="color-selector__list-item" data-value="2">red</li>
+  <li slot="item" class="color-selector__list-item" data-value="3">green</li>
+  <li slot="item" class="color-selector__list-item" data-value="4">blue</li>
+  <li slot="item" class="color-selector__list-item" data-value="1">black</li>
+  <li slot="item" class="color-selector__list-item" data-value="2">red</li>
+  <li slot="item" class="color-selector__list-item" data-value="3">green</li>
+  <li slot="item" class="color-selector__list-item" data-value="4">blue</li>
+</color-selector>
---
+++ js/color_selector.js
@@ -0,0 +1,51 @@
+class ColorSelector extends HTMLElement {
+  constructor() {
+    super();
+    this.selectTemplate = document.querySelector("#color-selector");
+    //trzeba stworzyc shadow, zeby uzywac slotow
+    this.attachShadow({ mode: "open" });
+    const clone = this.selectTemplate.content.cloneNode(true);
+    this.shadowRoot.appendChild(clone);
+    this.listElement = this.shadowRoot.querySelector('.select__list')
+    const slot = this.shadowRoot.querySelector("slot");
+    this.list = [...slot.assignedElements()];
+    this.selected = this.getAttribute("data-selected").split(',');
+
+    this.renderColors()
+    this.setEventListeners()
+  }
+
+  renderColors(){
+    this.list.forEach(el=>{
+      const value = el.getAttribute("data-value");
+      el.style.background = el.textContent;
+      el.textContent = ''
+      if(this.selected.includes(value)){
+        el.classList.add('color-selector__list-item--selected')
+      }
+    })
+  }
+
+  setEventListeners(){
+    this.list.forEach((el) => {
+      el.addEventListener("click", (_) => {
+        const value = el.getAttribute("data-value");
+        el.classList.toggle('color-selector__list-item--selected')
+        const valueIndex = this.selected.indexOf(value)
+        if(valueIndex !== -1){
+          this.selected.splice(valueIndex, 1)
+        }else{
+          this.selected.push(value)
+        }
+        this.dispatchEvent(new CustomEvent("selectionchange", {
+          detail: {
+            value: value,
+          },
+        }))
+      });
+    });
+  }
+}
+customElements.define("color-selector", ColorSelector);
+
+document.querySelector('.color-selector').addEventListener('selectionchange', e=> console.log(e))
