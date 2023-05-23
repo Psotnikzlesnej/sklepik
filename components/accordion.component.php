503e18979e7abb9ce2642c73557450079c3b8352
canicjusz <janek.bananek371@gmail.com>
2023-05-23 13:39:21 +0200

    #37 zorbione ale w sumie nie wiem co

---
+++ components/accordion.component.php
@@ -0,0 +1,36 @@
+<link rel="stylesheet" href="../styles/accordion.css">
+<aside class="sidebar">
+<p class="sidebar__heading">
+                <span class="sidebar__heading-text">Gniazdka, wyłączniki</span>
+                <a class="sidebar__btn sidebar__heading-btn icon" href="javascript:void(0);" onclick="toggleElementVisibility1()">
+                    <i class="sidebar__btnIcon plus" id="changeIcon1"></i>
+                </a>
+            </p>
+    <ul class="sidebar__list" id="sidebar__showHide1">
+        <li class="sidebar__listItem">
+        <a class="sidebar__listItemAnchor sidebar__listItemAnchor--active" href="#">
+                Berker
+                <i class="sidebar__btnIcon minus" id="changeIcon4"></i>
+</a>
+            <ul class="sidebar__nestedList">
+                <li class="sidebar__nestedListItem">
+                    <a class="sidebar__nestedListItemAnchor" href="#">
+                            test
+                    </a>
+                </li>
+                <li class="sidebar__nestedListItem">
+                    <a class="sidebar__nestedListItemAnchor" href="#">
+                        test
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="sidebar__listItem">
+            <a class="sidebar__listItemAnchor" href="#">
+                Berker
+                <i class="sidebar__btnIcon plus" id="changeIcon4"></i>
+            </a>
+        </li>
+    </ul>
+</aside>
+<script src="../js/accordion.js"></script>
---
+++ js/accordion.js
@@ -0,0 +1,9 @@
+// const spans = document.querySelectorAll('.sidebar__listItemSpan');
+// spans.forEach(el=>{
+//   el.addEventListener('click', e=>{
+//     const list = el.nextElementSibling
+//     if(list){
+//       list.classList.toggle('sidebar__nestedList--hidden')
+//     }
+//   })
+// })
