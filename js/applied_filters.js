5f288a4a45ad64cbff5ff32ce61dca0cf0cca464
mikolajczake <emilcancel@gmail.com>
2023-05-23 09:27:29 +0200

    #45 filtry

---
+++ js/applied_filters.js
@@ -0,0 +1,9 @@
+const removeButtons = document.querySelectorAll('.remove-button');
+removeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const word = button.getAttribute('data-word');
+        const wordContainer = button.parentNode;
+        wordContainer.remove();
+        console.log(`Usunięto słowo: ${word}`);
+    });
+});
