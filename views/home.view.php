d80d4808c76221b37f6d178930646f7cfba1f2da
mikolajczake <emilcancel@gmail.com>
2023-05-24 13:08:18 +0200

    #50 ukladanie home

--- views/home.view.php
+++ views/home.view.php
@@ -3,5 +3,8 @@
 
 require __DIR__ . '/../templates/header.php';
 require __DIR__ . '/../controllers/gallery.controller.php';
+require __DIR__ . '/../views/featured.view.php';
+require __DIR__ . '/../views/about_us.view.php';
+require __DIR__ . '/../templates/footer.php';
 
 ?>
