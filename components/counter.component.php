df1d64e34c4fc50433a92f71300c43e2c3fdacd1
canicjusz <janek.bananek371@gmail.com>
2023-05-24 14:39:10 +0200

    #55 LOL zrobione ^^

--- components/counter.component.php
+++ components/counter.component.php
@@ -5,4 +5,4 @@
   <input type="number" class="counter__input">
   <button type="button" class="counter__button counter__button-increase"></button>
 </template>
-<custom-counter class="counter" data-value="1" data-max="10" data-min="1"></custom-counter>
+<!-- <custom-counter class="counter" data-value="1" data-max="10" data-min="1"></custom-counter> -->
