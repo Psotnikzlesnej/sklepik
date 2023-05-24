20d47556d64b60cac7a895d81221c1e9262173d0
mikolajczake <emilcancel@gmail.com>
2023-05-24 13:09:42 +0200

    53 ukadanie porduct_display

---
+++ components/item.component.php
@@ -0,0 +1,26 @@
+
+<link rel="stylesheet" href="../styles/item.css">
+   
+
+</head>
+<body>
+ 
+
+     
+
+        <div class="product_tile">
+            <div class="photo">
+            </div>
+            <div class="product_tile-hover">
+                <div class="dsc">
+                    <div class="dsc1">TURBOMATURA</div>
+                    <div class="dsc2">2017</div>
+                </div>
+                <div class="price">
+                    <div class="price1">297zł</div>
+                    <div class="price2">100zł</div>
+                </div>
+     
+                </div>
+
+    </div>
---
+++ views/product_display.view.php
@@ -0,0 +1,72 @@
+
+<?php
+$url = $_SERVER['REQUEST_URI'];
+$url_manipulator = new URLManipulator($url);
+
+class URLManipulator {
+    public function __construct($url){
+        $this->url_parts = parse_url($url);
+        $queries = $this->url_parts['query'] ?? '';
+        parse_str($queries, $this->query_params);
+    }
+
+    public function setParam($key, ...$values){
+        $this->query_params[$key] = join(',', $values);
+    }
+
+    public function build_url(){
+        $encoded_query = http_build_query($this->query_params);
+        return $this->url_parts['path'] . '?' . $encoded_query;
+    }
+}
+
+function get_new_page_url($url_manipulator, $page) {
+    $url_manipulator->setParam('p', $page);
+    $new_url = $url_manipulator->build_url();
+    return $new_url;
+}
+
+
+function generateNavigation($pages, $currentPage, $middle_count = 3){
+	if ($pages < 1) return [];
+	$visiblePages = [$currentPage];
+	while (count($visiblePages) < $pages && count($visiblePages) < $middle_count) {
+		$firstElement = $visiblePages[0];
+		$lastElement = end($visiblePages);
+		if ($firstElement > 1) {
+			array_unshift($visiblePages, $firstElement - 1);
+		}
+		if ($lastElement < $pages) {
+			$visiblePages[] = $lastElement + 1;
+		}
+	}
+
+    if($visiblePages[0] == 2){
+        array_unshift($visiblePages, 1);
+    }else if ($visiblePages[0] > 2){
+        array_unshift($visiblePages, 1, '...');
+    }
+    if(end($visiblePages)+1 == $pages){
+        $visiblePages[] = $pages;
+    }else if (end($visiblePages)+1 < $pages){
+        array_push($visiblePages, '...', $pages);
+    }
+	return $visiblePages;
+};
+
+$current_page = 4;
+$pages_amount = 51;
+
+$pages_array = generateNavigation($pages_amount, $current_page);
+$current_index = array_search($current_page, $pages_array);
+$previous_page = $pages_array[$current_index-1] ?? NULL;
+$next_page = $pages_array[$current_index+1] ?? NULL;
+
+
+require __DIR__ . '/../components/product_navbar.component.php';
+require __DIR__ . '/../components/item.component.php';
+require __DIR__ . '/../components/item.component.php';
+require __DIR__ . '/../components/item.component.php';
+require __DIR__ . '/../components/product_navbar.component.php';
+
+?>
