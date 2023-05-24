0d5a1dc06963bf4a7ae6fbc5bf44cfc5d092384d
mikolajczake <emilcancel@gmail.com>
2023-05-24 13:37:43 +0200

    53 ukadanie porduct_display

--- components/item.component.php
+++ components/item.component.php
@@ -6,12 +6,15 @@
 <body>
  
 
-     
+    
 
         <div class="product_tile">
+            
             <div class="photo">
+        
             </div>
             <div class="product_tile-hover">
+            
                 <div class="dsc">
                     <div class="dsc1">TURBOMATURA</div>
                     <div class="dsc2">2017</div>
