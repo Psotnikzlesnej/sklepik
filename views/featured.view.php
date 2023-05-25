c75915b7b35242e2b74b195b4feb9b11110c9891
canicjusz <janek.bananek371@gmail.com>
2023-05-25 09:10:46 +0200

    #14 zrobione vjjngkhiuo

--- views/featured.view.php
+++ views/featured.view.php
@@ -9,31 +9,27 @@
 <div class="featured">
     <h2 class="featured__title">Polecane produkty</h2>
     <div class="featured__custom-break"></div>
-    <div class="carousel">
-        <div class="carousel__arrow carousel__arrow--left">
+    <div class="featured-carousel">
+        <div class="featured-carousel__arrow featured-carousel__arrow--left">
             <i class="icon-arrow-left"></i>
         </div>
-        <div class="carousel__product-container">
-            <div class="carousel__product"><?php include __DIR__ .'/../components/item.component.php' ?></div>
-            <div class="carousel__product"><?php include __DIR__ .'/../components/item.component.php' ?></div>
-            <div class="carousel__product"><?php include __DIR__ .'/../components/item.component.php' ?></div>
-            <div class="carousel__product"><?php include __DIR__ .'/../components/item.component.php' ?></div>
-            <div class="carousel__product"><?php include __DIR__ .'/../components/item.component.php' ?></div>
+        <div class="featured-carousel__product-container">
+            <div class="featured-carousel__element">
+                <div class="featured-carousel__product"><?php include __DIR__ .'/../components/item.component.php' ?></div>
+                <div class="featured-carousel__product"><?php include __DIR__ .'/../components/item.component.php' ?></div>
+                <div class="featured-carousel__product"><?php include __DIR__ .'/../components/item.component.php' ?></div>
+                <div class="featured-carousel__product"><?php include __DIR__ .'/../components/item.component.php' ?></div>
+                <div class="featured-carousel__product"><?php include __DIR__ .'/../components/item.component.php' ?></div>
+            </div>
+            <div class="featured-carousel__element">
+                <div class="featured-carousel__product"><?php include __DIR__ .'/../components/item.component.php' ?></div>
+                <div class="featured-carousel__product"><?php include __DIR__ .'/../components/item.component.php' ?></div>
+            </div>
         </div>
-        <div class="carousel__arrow carousel__arrow--right">
+        <div class="featured-carousel__arrow featured-carousel__arrow--right">
             <i class="icon-arrow-right"></i>
         </div>
     </div>
-    <a href="">Zobacz wszystkie inspiracje</a>
+    <a href="" class="featured__button">Zobacz wszystkie inspiracje</a>
 </div>
 <script src="js/featured.js"></script> 
-
-
-
-              
-              
-
-      
-
-                <div class="seeallproducts">ZOBACZ WSZYSTKIE INSPIRACJE</div>
-        </div>
