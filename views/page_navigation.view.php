7d558c14a9f13324360d6af7fb3ef2236bb62f4c
Nikrop <ltomasz989@gmail.com>
2023-05-29 09:25:32 +0200

    #36 ładnie ładnie ciekawie elegancko haj

--- views/page_navigation.view.php
+++ views/page_navigation.view.php
@@ -1,13 +1,11 @@
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= CSS_ROOT?>/page_navigation.css">
-<div class="nav-long">
-    
-    <div  class="cate">
-    <a class="hejka" data-link="description"><span class="myDiv">OPIS PRODUKTU</span></a>
-    <a class="hejka" data-link="parameters"><span class="myDiv">DANE TECHNICZNE</span></a>
-    <a class="hejka" data-link="other"><span class="myDiv">INNE Z KATEGORII</span></a>
-    </div>
-    </div>
 
-
-  <script src="<?= JS_ROOT?>/page_navigation.js"></script>
+    <div class="nav-long">
+        <div  class="cate">
+            <a class="hejka" data-link="description"><span class="myDiv">OPIS PRODUKTU</span></a>
+            <a class="hejka" data-link="parameters"><span class="myDiv">DANE TECHNICZNE</span></a>
+            <a class="hejka" data-link="other"><span class="myDiv">INNE Z KATEGORII</span></a>
+        </div>
+    </div>
+    <script src="<?= JS_ROOT?>/page_navigation.js"></script>
