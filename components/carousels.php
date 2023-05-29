fa9e1ed3d3042fce6a7b8981c0ee257af3ac3935
jacusdestroyer <drawskojacek@wp.pl>
2023-05-29 15:18:50 +0200

    #58 strzalki powrot

--- components/carousels.php
+++ components/carousels.php
@@ -1,14 +1,18 @@
 <link rel="stylesheet" href="<?= CSS_ROOT ?>/carousel_vertical.css">
 <link rel="stylesheet" href="<?= CSS_ROOT ?>/carousel_horizontal.css">
+<link rel="stylesheet" href="<?= CSS_ROOT ?>/icons.css">
 
 <div class="slider-wrapper">
   <button class="slide-arrow" id="slide-arrow-prev" >
-    <i class="fa-solid fa-angle-left" style="
+    <i class="icon-arrow-left"  style="
       display: block;
       margin-bottom: 0px;
       margin-left: 2px;
       margin-top: -2px;
-      margin-right: 0px;">
+      margin-right: 0px;
+       font-size: 15px;
+       color: white;">
+
     </i>
   </button>
 
@@ -26,19 +30,31 @@
   </ul>
 
   <button class="slide-arrow" id="slide-arrow-next">
-    <i class="fa-solid fa-angle-right" style="
+    <i class="icon-arrow-right" style="
       display: block;
       margin-bottom: 0px;
       margin-left: 2px;
       margin-top: -2px;
-      margin-right: 0px;">
+      margin-right: 0px;
+       font-size: 15px;
+       color: white;">
+
+
     </i>
   </button>
 </div>
 
 <section class="slider-wrapper_horizontal">
   <button class="slide-arrow_horizontal" id="slide-arrow-prev_horizontal">
-    <i class="fa-solid fa-angle-left"></i>
+  <i class="icon-arrow-left"  style="
+      display: block;
+      margin-bottom: 0px;
+      margin-left: 18px;
+      margin-top: 18px;
+      margin-right: 0px;
+       font-size: 15px;
+       color: white;">
+</i>
   </button>
 
   <ul class="slides-container_horizontal" id="slides-container_horizontal">
@@ -49,7 +65,14 @@
   </ul>
 
   <button class="slide-arrow_horizontal" id="slide-arrow-next_horizontal">
-    <i class="fa-solid fa-angle-right"></i>
+  <i class="icon-arrow-right" style="
+      display: block;
+      margin-bottom: 0px;
+      margin-left: 18px;
+      margin-top: 18px;
+      margin-right: 0px;
+       font-size: 15px;
+       color: white;"></i>
   </button>
   <script src="<?= JS_ROOT ?>/carousels.js"></script>
 
